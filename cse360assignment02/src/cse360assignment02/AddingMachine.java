47a4ae074ec5e28143a53c945832919615a9ce5b
orozc <orozc@DESKTOP-MNKG4A4>
2020-10-07 23:22:12 -0700

    First change: add history of subtraction and addition, and printed.

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -2,26 +2,50 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String history;
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    history= "0";
   }
   
   public int getTotal () {
-    return 0;
+	  return total;
   }
   
   public void add (int value) {
+	  total = total+value;
+	  history += " + " + value;
   }
 
   public void subtract (int value) {
+	  total = total-value;
+	  history += " - " + value;
   }
 
   public String toString () {
-    return "";
+    return history;
   }
  
 
   public void clear() {
   }
+
+  
+public static void main(String[] args) {
+    
+AddingMachine myAddingMachine= new AddingMachine();
+ 
+myAddingMachine.add(4);
+ 
+myAddingMachine.subtract(2);
+ 
+myAddingMachine.add(5);
+ 
+    System.out.println(myAddingMachine.toString());
+     
+    System.out.println("Total is: "+myAddingMachine.getTotal());
 }
+
+}
+
